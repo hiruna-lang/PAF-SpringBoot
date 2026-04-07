6178b8d77b420a65573b66ec747252018bd7dbbe
daneesha2003 <daneesha289@gmail.com>
2026-04-07 17:32:13 +0530

    Add Module 2 booking exception handling

---
+++ backend/backend/src/main/java/backend/Module_2/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package backend.Module_2.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
