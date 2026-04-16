959931bd9e1b49f013d624643d08b22c18152f7f
chamika217 <disanayakedilshan7@gmail.com>
2026-04-16 16:44:14 +0530

    feat: update App.js with Module 3 routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
 
 import Home from './Home/Home';
+import M3Module from './M3/m3';
 
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/m3/*" element={<M3Module/>}/>
 
     </Routes>
   );
