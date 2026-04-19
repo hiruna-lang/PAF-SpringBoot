12be1b2c6d6da8f90e2381ce41dcf99799fa603a
daneesha2003 <daneesha289@gmail.com>
2026-04-19 16:15:55 +0530

    delete resource repository

--- backend/backend/src/main/java/backend/Module_2/repository/ResourceRepository.java
+++
@@ -1,7 +0,0 @@
-package backend.Module_2.repository;
-
-import backend.Module_2.entity.Resource;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResourceRepository extends JpaRepository<Resource, Long> {
-}
