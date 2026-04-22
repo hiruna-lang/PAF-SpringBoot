993660140fef888a5332d9f6bde941c684f8aa28
kaveen12rt <kaveenpriyaboralessa@gmail.com>
2026-04-23 03:09:37 +0530

    Update UserRepository.java

--- backend/backend/src/main/java/backend/Module_4/Auth/repository/UserRepository.java
+++ backend/backend/src/main/java/backend/Module_4/Auth/repository/UserRepository.java
@@ -1,12 +1,15 @@
 package backend.Module_4.Auth.repository;
 
+import backend.Module_4.Auth.model.Role;
 import backend.Module_4.Auth.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByEmail(String email);
     Optional<User> findByGoogleId(String googleId);
     boolean existsByEmail(String email);
+    List<User> findByRole(Role role);
 }
